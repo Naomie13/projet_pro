8e01e4e5ecbbb66221b6bd18c1785380b153caec
Ange TCHEMMEGNE <tchemmegneange@gmail.com>
2026-05-09 21:39:50 +0200

    Ajoute des classes

---
+++ src/model/DrinkItem.java
@@ -0,0 +1,12 @@
+package model;
+
+public class DrinkItem extends MenuItem{
+
+	public DrinkItem(int id, String name, double price, String description) {
+		super(id, name, price, description);
+		// TODO Auto-generated constructor stub
+	}
+
+	
+
+}
