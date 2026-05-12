9960f58a480c6a475cdff00005eea33c6388bc40
Naomie13 <naomiealexandradomtchouang@gmail.com>
2026-05-12 14:09:59 +0200

    changement

--- src/service/MenuService.java
+++ src/service/MenuService.java
@@ -7,13 +7,33 @@ import java.util.List;
 
 public class MenuService {
 
-    private List<MenuItem> menu = new ArrayList<>();
+    private List<MenuItem> items;
 
-    public void addItem(MenuItem item) {
-        menu.add(item);
+    public MenuService() {
+        items = new ArrayList<>();
     }
 
-    public List<MenuItem> getMenu() {
-        return menu;
+    public MenuItem findById(int id) {
+
+        for (MenuItem item : items) {
+            if (item.getId() == id) {
+                return item;
+            }
+        }
+        return null;
+    }
+
+    public void updateItem(MenuItem item) {
+
+        for (int i = 0; i < items.size(); i++) {
+            if (items.get(i).getId() == item.getId()) {
+                items.set(i, item);
+                return;
+            }
+        }
+    }
+
+    public List<MenuItem> getAll() {
+        return items;
     }
 }
